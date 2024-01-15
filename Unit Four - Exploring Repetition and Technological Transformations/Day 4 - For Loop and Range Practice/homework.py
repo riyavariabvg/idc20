b1ef7da36b371b79c6987c1232fb177622b8bece
Riya Varia <rvaria@bayviewglen.ca>
2024-01-15 14:09:13 -0500

    for loop homework

---
+++ Unit Four - Exploring Repetition and Technological Transformations/Day 4 - For Loop and Range Practice/homework.py
@@ -0,0 +1,41 @@
+for num in range(10):
+    print(num + 1)
+
+for num in range(2, 12, 2):
+    print(num)
+
+sum = 0
+for num in range(1, 9, 2):
+    sum = sum + num
+
+print(sum)
+
+for num in range(10, 0, -1):
+    print(num)
+
+for num in range(4, 24, 4):
+    print(num)
+
+product = 1
+for num in range(1, 6):
+    product = num*product
+
+print(product)
+
+for num in range(3, 13):
+    if num % 3 == 0:
+        print(num)
+
+for num in range(5, -1, -1):
+    if num == 0:
+        print("Blast off!")
+    else:
+        print(num)
+
+for num in range (1,6):
+    print(num**2)
+
+for num in range(6):
+    print(2**num)
+
+    
